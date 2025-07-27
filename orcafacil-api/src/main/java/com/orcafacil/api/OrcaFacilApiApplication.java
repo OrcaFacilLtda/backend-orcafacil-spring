fd367fc91bc526e1d7564d40eba03df48515a44e
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2025-07-27 01:30:41 -0300

    feat(spring): adiciona projeto Spring inicial

---
+++ orcafacil-api/src/main/java/com/orcafacil/api/OrcaFacilApiApplication.java
@@ -0,0 +1,13 @@
+package com.orcafacil.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcaFacilApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcaFacilApiApplication.class, args);
+	}
+
+}
